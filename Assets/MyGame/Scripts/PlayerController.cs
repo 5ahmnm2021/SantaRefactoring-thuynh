aeba947974abf08c076646570ace41152fbb18c0
orvilline <thunhi.h@gmail.com>
2021-03-23 10:34:16 +0100

    refac: delete unsused method

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -15,11 +15,6 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
