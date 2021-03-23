8166af34662bb3e2d8357459782dda90d23d03a1
orvilline <thunhi.h@gmail.com>
2021-03-23 10:14:01 +0100

    refac: delete unsused name spaces

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
@@ -12,13 +10,7 @@ private void Awake()
     rb = GetComponent<Rigidbody2D>();
 }
 
-// Start is called before the first frame update
-void Start()
-{
-        
-}
 
-// Update is called once per frame
 void Update()
 {
     //if obstacle's position x is < -15f it will be destroyed
